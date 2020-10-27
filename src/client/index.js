3113ba3f1518b2c94ac7a605ac12231422a2457b
AM-Exercises <72548237+AM-Exercises@users.noreply.github.com>
2020-10-27 16:43:15 +0200

    Add files via upload

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,6 @@ import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
 
-import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/form.scss'
 import './styles/footer.scss'
